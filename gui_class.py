6746566df8b8e9c34aa5899f9f0d2b174dff230b
Bernhard <bernton96@gmail.com>
2021-11-04 09:15:42 +0100

    added customer and estimate position

--- gui_class.py
+++ gui_class.py
@@ -5,7 +5,7 @@ from tkinter import *
 
 
 class GUI_sim(tk.Tk):
-    def __init__(self):
+    def __init__(self, store_setup=""):
         super().__init__()
         # setup up root frame
         self.title('Supermarket')
@@ -14,10 +14,11 @@ class GUI_sim(tk.Tk):
         # setup supermarket frame
         self.SupM_frame = Frame(self)
         self.SupM_frame.pack(side = LEFT)
-        self.canvas = Canvas(self.SupM_frame, width = 620, height = 800, bg = self._color((230,230,230)))
-        self._SuperMarket_setup()
-        self.canvas.pack()
+        self.SupM_frame.canvas = Canvas(self.SupM_frame, width = 620, height = 800, bg = self._color((230,230,230)))
+        self._SuperMarket_setup(store_setup)
+        self.SupM_frame.canvas.pack()
 
+        """
         # setup info frame
         self.info_frame = Frame(self)
         self.info_frame.pack(side = RIGHT)
@@ -36,60 +37,76 @@ class GUI_sim(tk.Tk):
         
         self.text_area.grid(column = 0, pady = 10, padx = 10)
         self.text_area.configure(state = 'disable')
+        """
 
-    def _SuperMarket_setup(self):
-        #window shelvs
-        self.canvas.create_rectangle(0,0,40,600,width = 2, fill = self._color((140,140,140)))
-        self.canvas.create_rectangle(0,0,400,40,width = 2, fill = self._color((140,140,140)))
-        self.canvas.create_rectangle(480,0,620,40,width = 2, fill = self._color((140,140,140)))
-        self.canvas.create_rectangle(580,0,620,700,width = 2, fill = self._color((140,140,140)))
+    def _SuperMarket_setup(self,setup_file):
+        if setup_file is not  "":
+            self._SuperMarket_from_file(setup_file)
+        else:
+            #Set up standard store
+            #window shelvs
+            self.SupM_frame.canvas.create_rectangle(0,0,40,600,width = 2, fill = self._color((140,140,140)))
+            self.SupM_frame.canvas.create_rectangle(0,0,400,40,width = 2, fill = self._color((140,140,140)))
+            self.SupM_frame.canvas.create_rectangle(480,0,620,40,width = 2, fill = self._color((140,140,140)))
+            self.SupM_frame.canvas.create_rectangle(580,0,620,700,width = 2, fill = self._color((140,140,140)))
 
-        #create inner shelves
-        x0 = 120
-        while x0 < 600:
-            self.canvas.create_rectangle(x0,120,x0+40,300,width = 2, fill = self._color((140,140,140)))   
-            self.canvas.create_rectangle(x0,380,x0+40,600,width = 2, fill = self._color((140,140,140)))
-            x0 = x0 + 120
+            #create inner shelves
+            x0 = 120
+            while x0 < 600:
+                self.SupM_frame.canvas.create_rectangle(x0,120,x0+40,300,width = 2, fill = self._color((140,140,140)))   
+                self.SupM_frame.canvas.create_rectangle(x0,380,x0+40,600,width = 2, fill = self._color((140,140,140)))
+                x0 = x0 + 120
+
+    def _SuperMarket_from_file(self,setup_file):
+        f = open(setup_file)
+        lines = f.readlines()
+        print(lines)
 
     def _color(self,rgb):
         return "#%02x%02x%02x" % rgb 
 
 
     def add_msg(self, msg):
+        #DOESN'T work
         print(msg)
         print(tk.INSERT)
         self.text_area.insert("end",msg)
 
 
+    def place_customer(self,pos,cust_id):
+        self.SupM_frame.canvas.create_oval(pos[0]-5, pos[1]-5, pos[0]+5, pos[1]+5, fill = cust_id, width = 2)
+        self._draw_cross(pos[0]-10,pos[1]-10,cust_id)
+
+    def _draw_cross(self,x,y,cust_id):
+        self.SupM_frame.canvas.create_line(x,y,x+5,y+5, fill = cust_id, width = 2)
+        self.SupM_frame.canvas.create_line(x,y,x-5,y-5, fill = cust_id, width = 2)
+        self.SupM_frame.canvas.create_line(x,y,x+5,y-5, fill = cust_id, width = 2)
+        self.SupM_frame.canvas.create_line(x,y,x-5,y+5, fill = cust_id, width = 2)
+        
 
 
-#root = Tk()
-#root.title('Supermarket')
-
-#canvas = Canvas(root, width = 600, height = 800, bg = "#%02x%02x%02x" % (230,230,230))
-#canvas.pack()
-
-#for i in range(40) : # Vertical Lines
-#    x = 40 + (i * 40)
-#    canvas.create_line(x,800,x,-800,width = 2)
-
-
-#for i in range(40) : # Horizontal Line
-#    y = 40 - (i * 40)
-#    canvas.create_line(600,-y,10,-y,width = 2)
-
-
-#canvas.create_rectangle(0,0,40,600,width = 2, fill = "#%02x%02x%02x" % (140,140,140))
-#canvas.create_rectangle(0,0,600,40,width = 2, fill = "#%02x%02x%02x" % (140,140,140))
-
-#root.mainloop()
 
 if __name__ == "__main__":
     gui = GUI_sim()
+
+    PATH_1 = [(20,650),(80,650)]
+    
+    
+    gui.place_customer(PATH_1[0],"red")
+    
+    #for i in range(1,100):
+        
+
+
+
+
+     #   time.sleep(1)
+    
     gui.mainloop()
-    my_msg = "Hello World "
-    for i in range(100):
-        gui.add_msg(my_msg + str(i))
-        time.sleep(2)
+
+    #my_msg = "Hello World "
+    #for i in range(100):
+    #    gui.add_msg(my_msg + str(i))
+    #    time.sleep(2)
 
 
